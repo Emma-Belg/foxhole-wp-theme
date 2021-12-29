90bdc59ea42844fffb3219cecdce117bfa0f3834
emmaw <emma.wishart@live.com.au>
2021-12-29 17:52:27 +0100

    adding work categories carousel and testimonial slider to homepage + relevant functions and styling

--- functions.php
+++ functions.php
@@ -15,10 +15,10 @@
 //    wp_enqueue_style( 'twentytwenty-style', get_template_directory_uri() . '/style.css' );
 //});
 
-add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+add_action('wp_enqueue_scripts', 'my_theme_enqueue_styles');
 function my_theme_enqueue_styles() {
-    wp_enqueue_style( 'child-style', get_stylesheet_uri(),
-        array( 'twenty-twenty-one-style' ),
+    wp_enqueue_style('child-style', get_stylesheet_uri(),
+        array('twenty-twenty-one-style'),
         wp_get_theme()->get('Version') // this only works if you have Version in the style header
     );
 }
@@ -27,7 +27,8 @@ function display_homepage_video() {
     ?>
     <div class="video-background">
         <div class="video-foreground">
-            <iframe src="https://www.youtube.com/embed/W0LHTWG-UmQ?controls=0&rel=0&autoplay=1&mute=1&loop=1&playlist=W0LHTWG-UmQ" frameborder="0" allowfullscreen></iframe>
+            <iframe src="https://www.youtube.com/embed/W0LHTWG-UmQ?controls=0&rel=0&autoplay=1&mute=1&loop=1&playlist=W0LHTWG-UmQ"
+                    frameborder="0" allowfullscreen></iframe>
         </div>
     </div>
 
@@ -40,5 +41,143 @@ function display_homepage_video() {
         </div>
     </div>
     <br/>
+    <?php
+}
+
+function echoString($string) {
+    echo $string;
+}
+
+function getSpecificValueOfChildrenFromParentCategory($parentCategoryID, $value): array {
+    $args = array(
+        'orderby' => 'name',
+        'order' => 'ASC',
+        'parent' => $parentCategoryID,
+        'hide_empty' => false,
+    );
+    $categories = get_categories($args);
+    $categoryValue = [];
+    foreach ($categories as $category) {
+        array_push($categoryValue, $category->$value);
+    }
+    return $categoryValue;
+}
+
+function getPostFromCategory($categoryID): array {
+    $args = array(
+        'post_type' => 'post',
+        'post_status' => 'publish',
+        'category' => $categoryID,
+        'numberposts' => 1,
+    );
+    $posts = get_posts($args);
+    return $posts;
+}
+
+function getChildrenPostsFromCategory($arrayFromFunction): array
+{
+    $arrayOfChildrenPosts = [];
+    foreach ($arrayFromFunction as $childPost) {
+        array_push($arrayOfChildrenPosts, getPostFromCategory($childPost));
+    }
+    return $arrayOfChildrenPosts;
+}
+
+function fadingCarouselCategories($inputCategoryID, $parentID = null)
+{
+    $categories = get_categories($inputCategoryID);
+    $parentName = get_category($parentID);
+    ?>
+    <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+    <a class="next" onclick="plusSlides(1)">&#10095;</a>
+    <div class="slideshow-container">
+    <?php
+    foreach ($categories as $category) {
+        if ($category->name !== "Uncategorized" && $category->name !== $parentName->name) {
+            $args = array(
+                'post_type' => 'post',
+                'post_status' => 'publish',
+                'category_name' => $category->name,
+                'posts_per_page' => 1,
+            );
+            $arr_posts = new WP_Query($args);
+            $current_url = home_url($_SERVER['REQUEST_URI']);
+            if ($arr_posts->have_posts()) :
+                $arr_posts->the_post();
+                if (has_post_thumbnail()) :
+                    echoString('
+                   <div class="mySlides">
+                      <img width="100%" height="auto" alt="' . $category->slug . '" 
+                          src="' . get_the_post_thumbnail_url() . '">
+                      <a href="' . $current_url . '/' . $category->slug . '">
+                         <div class="text">' . $category->slug . '</div>
+                      </a>
+                   </div>');
+                endif;
+            endif;
+        }
+    }
+    ?>
+    </div>
+    <script>
+        let slideIndex = 1;
+        let myTimer;
+        let slideshowContainer;
+
+        window.addEventListener("load", function () {
+            showSlides(slideIndex);
+            myTimer = setInterval(function () {
+                plusSlides(1)
+            }, 4000);
+        })
+
+        // NEXT AND PREVIOUS CONTROL
+        function plusSlides(index) {
+            clearInterval(myTimer);
+            if (index < 0) {
+                showSlides(slideIndex -= 1);
+            } else {
+                showSlides(slideIndex += 1);
+            }
+
+            if (index === -1) {
+                myTimer = setInterval(function () {
+                    plusSlides(index + 2)
+                }, 4000);
+            } else {
+                myTimer = setInterval(function () {
+                    plusSlides(index + 1)
+                }, 4000);
+            }
+        }
+
+        //Controls the current slide and resets interval if needed
+        function currentSlide(index) {
+            clearInterval(myTimer);
+            myTimer = setInterval(function () {
+                plusSlides(index + 1)
+            }, 4000);
+            showSlides(slideIndex = index);
+        }
+
+        function showSlides(index) {
+            let i;
+            let slides = document.getElementsByClassName("mySlides");
+            if (index > slides.length) {
+                slideIndex = 1
+            }
+            if (index < 1) {
+                slideIndex = slides.length
+            }
+            for (i = 0; i < slides.length; i++) {
+                slides[i].style.display = "none";
+            }
+            for (i = 0; i < slides.length; i++) {
+                slides[i].className = slides[i].className.replace(" fade", "");
+            }
+            slides[slideIndex - 1].style.display = "inline";
+            slides[slideIndex-1].className += " fade";
+        }
+    </script>
     <?php
 }
--- page-templates/template-home.php
+++ page-templates/template-home.php
@@ -15,7 +15,7 @@ get_header();
 
     <div class="between-paper first-divide">
         <img width=100% alt="ripped paper"
-             src="https://staging.foxholefilms.com.au/wp-content/themes/foxy/assets/images/textures/Torn-Paper-Edge-03-overdark.png">
+             src="https://staging.foxholefilms.com.au/wp-content/themes/foxy/assets/images/textures/Torn-Paper-Edge-02-underfox.png">
     </div>
     <div class="homepage-section">
         <div class="intro-about">
@@ -26,7 +26,7 @@ get_header();
             <div class="flex-container">
                 <div class="right-float-button">
                     <a href="/about">
-                        <button>About Us</button>
+                        <button class="button-bottom-margin">About Us</button>
                     </a>
                 </div>
             </div>
@@ -35,6 +35,10 @@ get_header();
 
 
     <!--featured work section-->
+    <div class="halftone">
+        <img width=100% alt="ripped paper"
+             src="https://staging.foxholefilms.com.au/wp-content/themes/foxy/assets/images/textures/Distressed-Halftone-Texture-02-horizontal.png">
+    </div>
     <div class="featured-section">
         <div class="flex-container">
             <div class="block">
@@ -106,23 +110,72 @@ get_header();
     <div class="work-section">
         <div class="flex-container">
             <div class="flex-row">
-                <div class="work-box">
-                    <div class="work-img">
-                        <img width="100%" alt="featured-work1"
-                             src="https://staging.foxholefilms.com.au/wp-content/themes/foxy/assets/images/work/FoxHoleFilmsProperty1.png">
-                        <div class="featured-content">
-                            <h3>Title</h3>
-                            <p>A short description of the work goes here.</p>
-                            <a href="#">see more</a>
-                        </div>
-                    </div>
-                    <?php
-                    echo get_the_content(null, false, 24);
+                <h3><?php
+                    echo get_the_title(24);
                     ?>
+                </h3><br/><div class="work-box">
+
+                    <div class="float-right">
+                        <?php
+                        fadingCarouselCategories(getChildrenPostsFromCategory(getSpecificValueOfChildrenFromParentCategory(3, 'cat_ID')), 3);
+                        ?>
+                    </div>
+                    <div class="work-content">
+                        <?php
+                        echo get_the_content(null, false, 24);
+                        ?>
+                    </div>
                 </div>
             </div>
         </div>
-        <div class="block">
+        <div class="work-section">
+            <div class="work-box">
+                <h3><?php echo get_the_title(24); ?></h3>
+
+            </div>
+        </div>
+
+        <div class="block full-width">
+            <div class="horizontal-gallery">
+                <div class="flex">
+                    <div class="category-img">
+                        <a href="#">
+                            <img width="300px" alt="featured-work1"
+                                 src="https://staging.foxholefilms.com.au/wp-content/themes/foxy/assets/images/work/FoxHoleFilmsProperty1.png">
+                            <div class="category-title">
+                                <h3>Property</h3>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="category-img">
+                        <a href="#">
+                            <img width="300px" alt="featured-work1"
+                                 src="https://staging.foxholefilms.com.au/wp-content/themes/foxy/assets/images/work/FoxHoleFilmsProperty1.png">
+                            <div class="category-title">
+                                <h3>Corporate</h3>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="category-img">
+                        <a href="#">
+                            <img width="300px" alt="featured-work1"
+                                 src="https://staging.foxholefilms.com.au/wp-content/themes/foxy/assets/images/work/FoxHoleFilmsProperty1.png">
+                            <div class="category-title">
+                                <h3>Commercial</h3>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="category-img">
+                        <a href="#">
+                            <img width="300px" alt="featured-work1"
+                                 src="https://staging.foxholefilms.com.au/wp-content/themes/foxy/assets/images/work/FoxHoleFilmsProperty1.png">
+                            <div class="category-title">
+                                <h3>Film & Documentary</h3>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
             <img width="100%" alt="featured-work1"
                  src="https://staging.foxholefilms.com.au/wp-content/themes/foxy/assets/images/textures/Torn-Paper-Edge-16-double.png">
         </div>
@@ -206,6 +259,34 @@ get_header();
     <div class="testimonial-section">
         <div class="intro-our-work">
             <h3>Brands we've worked with</h3>
+            <div class="slider">
+                <input type="radio" name="slider" title="slide1" checked="checked" class="slider__nav"/>
+                <input type="radio" name="slider" title="slide2" class="slider__nav"/>
+                <input type="radio" name="slider" title="slide3" class="slider__nav"/>
+                <input type="radio" name="slider" title="slide4" class="slider__nav"/>
+                <div class="slider__inner">
+                    <div class="slider__contents">
+                        <p class="slider__image">&#129409;</p>
+                        <p class="testimonial-slider-brand"><b>Lion Brand</b></p>
+                        <p class="slider__txt">Lorem ipsum dolor sit amet, consectetur adipisicing elit. At cupiditate omnis possimus illo quos, corporis minima!</p>
+                    </div>
+                    <div class="slider__contents">
+                        <p class="slider__image">&#9757;</p>
+                        <p class="testimonial-slider-brand"><b>That's my point</b></p>
+                        <p class="slider__txt">Lorem ipsum dolor sit amet, consectetur adipisicing elit. At cupiditate omnis possimus illo quos, corporis minima!</p>
+                    </div>
+                    <div class="slider__contents">
+                        <p class="slider__image">&#9996;</p>
+                        <p class="testimonial-slider-brand"><b>Peace</b></p>
+                        <p class="slider__txt">Lorem ipsum dolor sit amet, consectetur adipisicing elit. At cupiditate omnis possimus illo quos, corporis minima!</p>
+                    </div>
+                    <div class="slider__contents">
+                        <p class="slider__image">&#10022;</p>
+                        <p class="testimonial-slider-brand"><b>Diamond</b></p>
+                        <p class="slider__txt">Lorem ipsum dolor sit amet, consectetur adipisicing elit. At cupiditate omnis possimus illo quos, corporis minima!</p>
+                    </div>
+                </div>
+            </div>
             <p>Maybe some testimonials that flash through here</p>
             <p>Then some of the brands' logos</p>
         </div>
